c6cb7ac427ce64f984bd4029df19ff5c9f69525c
Dennis Hernández <dennishernandezvargas@gmail.com>
2014-10-29 09:50:08 -0600

    Adding main functionalities

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ flatJSON
 [![Build Status](https://travis-ci.org/djhvscf/flatJSON.svg?branch=master)](https://travis-ci.org/djhvscf/flatJSON)
 
 This plugin creates a Flat JSON. You can pass the URL or object to convert to JSON
+
+### Example
+{"foo":{"bar":[0]}} => {"foo.bar.0":0}
+
--- src/flatJSON.js
+++ src/flatJSON.js
@@ -35,8 +35,95 @@
 
 ;(function ($) {
     'use strict'
-    $.fn.flatJSON = function (options) {
+    $.flatJSON = function (options) {
 		
+		var defaults = {
+			url: '',
+			data: {},
+			flat: false,
+			unFlat: false, 
+			success: function(obj) { console.log(obj.data); console.log(sd.flatHelper(obj.data)) },
+			method: 'GET',
+			dataType: 'JSONP',
+			crossDomain: true
+		}, options = $.extend(defaults, options);
 		
+		var sd = {
+			flat: function(data) {
+				var result = {};
+				function recurse (cur, prop) {
+					if (Object(cur) !== cur) {
+						result[prop] = cur;
+					} else if (Array.isArray(cur)) {
+						for(var i = 0, l = cur.length; i < l; i++) {
+							recurse(cur[i], prop ? prop+"."+i : ""+i);
+							if (l == 0) {
+								result[prop] = [];
+							}
+						}
+					} else {
+						var isEmpty = true;
+						for (var p in cur) {
+							isEmpty = false;
+							recurse(cur[p], prop ? prop+"."+p : p);
+						}
+						if (isEmpty) {
+							result[prop] = {};
+						}
+					}
+				}
+				recurse(data, "");
+				return result;
+			},
+
+			unflat: function(data) {
+				"use strict";
+				if (Object(data) !== data || Array.isArray(data)) {
+					return data;
+				}
+				var result = {}, cur, prop, idx, last, temp;
+				for(var p in data) {
+					cur = result, prop = "", last = 0;
+					do {
+						idx = p.indexOf(".", last);
+						temp = p.substring(last, idx !== -1 ? idx : undefined);
+						cur = cur[prop] || (cur[prop] = (!isNaN(parseInt(temp)) ? [] : {}));
+						prop = temp;
+						last = idx + 1;
+					} while(idx >= 0);
+					cur[prop] = data[p];
+				}
+				return result[""];
+			},
+			
+			flatHelper: function (data) {
+				var flatArray = [];
+				$.each(data, function(i, element) {
+					flatArray.push(sd.flat(element));
+				});
+				return flatArray;
+			}
+		};
+		
+		var init = function () {
+			if(options.url !== '') {
+				$.ajax({
+					url: options.url,
+					crossDomain: options.crossDomain,
+					dataType: options.dataType,
+					type: options.method,
+					success: options.success,
+				});
+				
+			} else if (options.data !== {}) {
+				if(optins.flat) {
+					return sd.flatHelper(options.data);
+				} else if(options.unFlat) {
+					//return sd.unflat(options.data);
+				}
+			}			
+		};
+		
+		init();
     };
 })(jQuery);
